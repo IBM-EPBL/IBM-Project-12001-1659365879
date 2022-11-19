a22706e3ed219c13ce90be60a9d6abc8388b92e5
ranjithrr2001 <67401880+ranjithrr2001@users.noreply.github.com>
2022-11-19 13:47:57 +0530

    Updated files after deployment

--- Project Development Phase/Sprint 4/app.py
+++ Project Development Phase/Sprint 4/app.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template, request
 from math import ceil
 import pickle
+import requests
 
 app = Flask(__name__)
-model = pickle.load(open("model1.pkl","rb"))
+# model = pickle.load(open("model1.pkl","rb"))
+
+# fetching the ML model from the IBM Deployment space
+
+API_KEY = "TpADwrsyFnYatIlV70EKAo9NQtUrr7HaZsR7TUREFkMR"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={
+    "apikey":API_KEY, 
+    "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'
+})
+mltoken = token_response.json()["access_token"]
+header = {
+    'Content-Type': 'application/json', 
+    'Authorization': 'Bearer ' + mltoken
+}
+
 
 @app.route('/')
 def home():
@@ -22,11 +37,24 @@ def predict():
         research = 1
     else:
         research = 0
-    pred = [[gre,toefl,rating,sop,lor,cgpa,research]]
-    result=model.predict(pred)
+    
+    payload_scoring = {"input_data": [{
+        "fields": [["GRE Score","TOEFL Score","University Rating","SOP","LOR","CGPA","Research"]], 
+        "values": [[gre,toefl,rating,sop,lor,cgpa,research]]
+    }]}
+    
+    # fetching the prediction result..
+    
+    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/b47f221e-15b5-4c30-a265-8174210f27b3/predictions?version=2022-11-19', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
+    # print("Scoring response")
+    # print(response_scoring.json())
+    
+    temp = response_scoring.json()
+    result = temp["predictions"][0]['values'][0][0]
+    
     if result > 0.5:
-        return render_template("chance.html",msg="Congratulation, you are eligible. As the probability is "+str(ceil(result[0]*100))+"%")
-    return render_template("nochance.html",msg="Sorry, Unfortunately you aren't eligible. As the probability is "+str(ceil(result[0]*100))+"%")
+        return render_template("chance.html",msg="Congratulation, you are eligible. As the probability is " + str(ceil(result*100))+"%")
+    return render_template("nochance.html",msg="Sorry, Unfortunately you aren't eligible. As the probability is " + str(ceil(result*100))+"%")
 
 if __name__ == "__main__":
-    app.run(port=8801)
+    app.run(debug=True)
